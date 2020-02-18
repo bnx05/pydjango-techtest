28eab251416d399ce1381fae28871239eacd93d5
bnx05 <banquil_05ive@yahoo.com>
2020-02-18 22:57:11 +0800

    final implementation for feature 1

--- imagedb/images/admin.py
+++ imagedb/images/admin.py
@@ -1,28 +1,21 @@
 from django.contrib import admin
-from .forms import ImageForm
+
 from .models import Image, ImageLabel
+
+
+class ImageLabelInline(admin.TabularInline):
+    extra = 0
+    min_num = 1
+    model = ImageLabel
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
+    inlines = [ImageLabelInline]
     list_display = ('id', 'labels')
-    form = ImageForm
 
     def labels(self, obj):
         return list(obj.labels.all())
-
-    def save_model(self, request, obj, form, change):
-        label = form.cleaned_data['label_field']
-        label_list = label.split(",")
-
-        def create_img_label(obj, labels_to_create):
-            for label in label_list:
-                img_label = ImageLabel(image=obj, label=label)
-                img_label.save()
-
-        if not obj.id:
-            obj.save()
-        create_img_label(obj, label_list)
 
 
 @admin.register(ImageLabel)
